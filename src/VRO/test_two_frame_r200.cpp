91410d5c944b633dd666717d0fbd7407b5c6adc0
He Zhang <hxzhang1@ualr.edu>
2017-10-08 11:08:47 -0500

    Add histogram equalization in test_two_frame_r200

--- src/VRO/test_two_frame_r200.cpp
+++ src/VRO/test_two_frame_r200.cpp
@@ -10,6 +10,8 @@
 #include <string> 
 #include <tf/tf.h>
 #include <iostream>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
 #include "sparse_feature_vo.h"
 // #include "SR_reader_cv.h"
 #include "rs_r200_wrapper.h"
@@ -24,6 +26,9 @@ void testTwoFrameMatch();
 // void map_raw_img_to_grey(unsigned short * pRaw, unsigned char* pGrey, int N);
 void print_tf(ostream& out, tf::Transform tT); 
 void init_parameters(); 
+
+cv::Mat rgb2gray(cv::Mat rgb); 
+cv::Mat histogram_equal(cv::Mat m); 
 
 int main(int argc, char* argv[])
 {
@@ -49,7 +54,8 @@ void testTwoFrameMatch()
   float d = 1.; // 2.;
   // CamModel c2h(608.1673/d, 605.7/2,  323.717/d, 228.8423/d, 0.18159, -0.60858); 
 
-  CamModel c2h(313.8326, 316.9582, 153.7794, 118.7975, -0.0845, 0.05186);
+  CamModel c2h(313.8326, 316.9582, 153.7794, 118.7975, -0.0845, 0.05186); // this is rgb camera's intrinsic parameters
+  // CamModel c2h(309.1375, 309.1375, 156.3687, 119.4300); // this is depth camera's intrinsic parameters  
   c2h.width = 320; // 640 
   c2h.height = 240; // 480
 
@@ -63,8 +69,8 @@ void testTwoFrameMatch()
   // generate imgs and dpts 
   cv::Mat from_dpt; // (SR_HEIGHT, SR_WIDTH, CV_16UC1); 
   cv::Mat to_dpt;   // (SR_HEIGHT, SR_WIDTH, CV_16UC1); 
-  cv::Mat from_grey; 
-  cv::Mat to_grey; 
+  cv::Mat from_gray; 
+  cv::Mat to_gray; 
 
   CCameraNode::gb_dis_match_point = true; // whether to see the result of 
   CSparseFeatureVO spr_vo(c2h); 
@@ -74,7 +80,7 @@ void testTwoFrameMatch()
   f_from_rgb = file_dir + "/color/" + match_from; 
   f_from_dpt = file_dir + "/depth/" + match_from; 
 
-  if(!rs2h.readOneFrameCV(f_from_rgb, f_from_dpt, from_grey, from_dpt))
+  if(!rs2h.readOneFrameCV(f_from_rgb, f_from_dpt, from_gray, from_dpt))
   {
     ROS_INFO("%s failed to read files: %s frame %s ", __FILE__, f_from_rgb.c_str(), f_from_dpt.c_str()); 
     return ;
@@ -87,19 +93,48 @@ void testTwoFrameMatch()
   f_to_rgb = file_dir + "/color/" + match_to;
   f_to_dpt = file_dir + "/depth/" + match_to; 
 
-  if(!rs2h.readOneFrameCV(f_to_rgb, f_to_dpt, to_grey, to_dpt )) 
+  if(!rs2h.readOneFrameCV(f_to_rgb, f_to_dpt, to_gray, to_dpt )) 
   {
     ROS_INFO("%s failed to read files : %s frame %s ", __FILE__, f_to_rgb.c_str(), f_to_dpt.c_str()); 
     return ;
   }
 
-  cv::imshow("to img", to_grey); 
+  // add histogram equalization 
+  cv::Mat from_heq, to_heq; 
+  from_gray = rgb2gray(from_gray); 
+  to_gray = rgb2gray(to_gray); 
+  
+  from_heq = histogram_equal(from_gray); 
+  to_heq = histogram_equal(to_gray); 
+
+  cv::imshow("to img", to_heq); 
   cv::waitKey(10); 
 
-  tf::Transform tran = spr_vo.VRO(to_grey, to_dpt, from_grey, from_dpt, dpt_scale); 
+  // tf::Transform tran = spr_vo.VRO(to_gray, to_dpt, from_gray, from_dpt, dpt_scale); 
+  tf::Transform tran = spr_vo.VRO(to_heq, to_dpt, from_heq, from_dpt, dpt_scale); 
 
   print_tf(std::cout, tran); 
   return ; 
+}
+
+cv::Mat rgb2gray(cv::Mat rgb)
+{
+  cv::Mat gray_img; 
+  if(rgb.type() == CV_8UC3)
+  {
+    cvtColor(rgb, gray_img, CV_RGB2GRAY);
+  }else{
+    gray_img = rgb.clone(); 
+  }
+  return gray_img; 
+}
+
+cv::Mat histogram_equal(cv::Mat m)
+{
+  cv::Mat ret; 
+  cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(3.0, cv::Size(8, 8));
+  clahe->apply(m, ret); 
+  return ret;
 }
 
 
@@ -110,8 +145,8 @@ void init_parameters()
   pP->m_feature_detector_type = "SIFT";  // SIFTGPU 
   pP->m_feature_descriptor_type = "SIFT";  // SIFTGPU
   pP->m_feature_match_type = "FLANN"; 
-  pP->m_nn_distance_ratio = 0.4; // 0.5 0.95 for SIFTGPU, 0.5-0.7 for SIFT 
-  pP->m_max_dist_for_inliers = 0.07; // 0.05  
+  pP->m_nn_distance_ratio = 0.8; // 0.5 0.95 for SIFTGPU, 0.5-0.7 for SIFT 
+  pP->m_max_dist_for_inliers = 0.05; // 0.05  
   pP->m_max_num_features = 5000; 
   pP->m_ransac_iterations = 5000; 
   pP->m_sift_num_features = 0; 
